37ad57b3f00b7815d192900b97fe50263158c966
Damien Garros <dgarros@gmail.com>
2021-06-22 11:54:35 -0400

    Update examples/example3/nautobot_adapter.py
    Co-authored-by: Glenn Matthews <glenn.matthews@networktocode.com>

--- examples/example3/nautobot_adapter.py
+++ examples/example3/nautobot_adapter.py
@@ -109,7 +109,7 @@ class NautobotCountry(Country):
 
 
 class NautobotAdapter(DiffSync):
-    """Example of a DiffSync adapter implementation."""
+    """Example of a DiffSync adapter implementation using pynautobot to communicate with a remote Nautobot system."""
 
     # We are using NautobotCountry and NautobotRegion instead of Region and Country
     # because we are using these classes to manage the logic to integrate with Nautobot
